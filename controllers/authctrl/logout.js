bc99b3af034ceb4988f853c1cb49d050b954f193
Emmanuel Giraldo <esemmanuel2022@gmail.com>
2023-10-10 01:21:14 -0500

    changes

--- controllers/authctrl/logout.js
+++ controllers/authctrl/logout.js
@@ -1,25 +1,43 @@
+const passport = require("passport");
+// const auth = require("./auth"); // Importa el middleware 'auth'
+const validToken = require("../../midddleware/validToken"); // Importa el middleware 'validToken'
 const invalidatedTokens = new Set();
-
 const logOutctrl = async (req, res, next) => {
-  const authHeader = req.headers.authorization;
-  const token = authHeader && authHeader.split(" ")[1];
+  // Utiliza el middleware 'auth' para asegurarte de que el usuario esté autenticado
+  
+  passport.authenticate("jwt", { session: false }, (err, user) => {
+    if (!user || err) {
+      return res.status(401).json({
+        status: "error",
+        code: 401,
+        message: "Unauthorized",
+      });
+    }
+    req.user = user;
 
-  if (token) {
-    invalidatedTokens.add(token);
-    console.log(Array.from(invalidatedTokens));
-    res.status(204).json({
-      status: "success",
-      code: 200,
-      message: "Successfully logout",
-      data: "Success",
+
+    validToken(req, res, () => {
+      const authHeader = req.headers.authorization;
+      const token = authHeader && authHeader.split(" ")[1];
+
+      if (token) {
+        invalidatedTokens.add(token);
+        console.log(Array.from(invalidatedTokens));
+        res.status(204).json({
+          status: "success",
+          code: 204,
+          message: "Successfully logged out",
+          data: "Success",
+        });
+      } else {
+        res.status(400).json({
+          status: "error",
+          code: 400,
+          message: "Invalid token",
+        });
+      }
     });
-  } else {
-    res.status(400).json({
-      status: "error",
-      code: 400,
-      message: "Invalid token",
-    });
-  }
+  })(req, res, next);
 };
 
 module.exports = logOutctrl;
